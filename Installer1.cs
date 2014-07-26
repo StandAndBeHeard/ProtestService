227f88812b789314a1bb6ac2b09b93d2f205d359
Jeremy Zongker <jeremy@trilitech.com>
2014-07-25 19:21:08 -0500

    Initial Commit

---
+++ Installer1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProtestService
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+
+        public Installer1()
+        {
+            InitializeComponent();
+
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceInstaller1.ServiceName = "Protest Service";
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] { this.serviceProcessInstaller1, this.serviceInstaller1 });
+        }
+
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtestService
+{
+    public partial class Service1 : ServiceBase
+    {
+        System.Timers.Timer timer5;     //every 5 min
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer5 = new System.Timers.Timer(300000); //5 minutes
+            timer5.Elapsed += new System.Timers.ElapsedEventHandler(timer5_Elapsed);
+            timer5.Start();
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        void timer5_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer5.Stop();
+            try {
+                ProtestLib.Protests.UpdateStatus();
+                ProtestLib.Notifications.SendPending();
+            }
+            catch { }
+            timer5.Start();
+        }
+
+
+
+    }
+}
